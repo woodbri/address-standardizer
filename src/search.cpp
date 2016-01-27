5470b7515ab944090bc8f6e8f5ecc3542d115575
Stephen Woodbridge <woodbri@swoodbridge.com>
2016-01-27 16:19:24 -0500

    Updated LICENSE file and added headers to all source files.

--- src/search.cpp
+++ src/search.cpp
@@ -1,5 +1,15 @@
-/*HEADER*
-*HEADER*/
+/**ADDRESS_STANDARDIZER***************************************************
+ *
+ * Address Standardizer
+ *      A collection of C++ classes for parsing street addresses
+ *      and standardizing them for the purpose of Geocoding.
+ *
+ * Copyright 2016 Stephen Woodbridge <woodbri@imaptools.com>
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the MIT License. Please file LICENSE for details.
+ *
+ ***************************************************ADDRESS_STANDARDIZER**/
 
 #include <iostream>
 
